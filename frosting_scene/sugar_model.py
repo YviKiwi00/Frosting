16ea9f4b422def75896e6bed95e7e15f121409a2
Yvette Gyarmati <yvette@yvette-gyarmati.de>
2025-03-10 20:55:30 +0100

    updated rasterizer because segmentation changes to diff_rasterizer

--- frosting_scene/sugar_model.py
+++ frosting_scene/sugar_model.py
@@ -2314,7 +2314,7 @@ class SuGaR(nn.Module):
                 print("scales", scales.shape)
             print("screenspace_points", screenspace_points.shape)
         
-        rendered_image, radii = rasterizer(
+        rendered_image, radii, _, _, _ = rasterizer(
             means3D = positions,
             means2D = means2D,
             shs = shs,
